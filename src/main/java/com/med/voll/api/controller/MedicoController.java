17ff7ffb4270b02a15613a753a8e913144fc4a3e
fantestiQ <isac3539@gmail.com>
2025-08-12 18:27:49 -0300

    first commit

---
+++ src/main/java/com/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,13 @@
+package com.med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("medicos")
+public class MedicoController {
+
+    @PostMapping
+    public void cadastrar(@RequestBody String json){
+        System.out.println(json);
+    }
+}
